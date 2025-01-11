6133248968a209ded00b18ea11679bcd7be7a1a1
Jeferson Douglas <jefersonmedeiros000@gmail.com>
2025-01-10 23:39:41 -0300

    Column namming definitive generalization
    This cicle will repeat many times. The solution is generalized in
    small steps , but in some point a definitive general solution needs
    to come up to, in order to satisfy one of the basics rules of TDD:
    as the tests gets more specific, the code gets more generic.
    
    Signed-off-by: Jeferson Douglas <jefersonmedeiros000@gmail.com>

--- square.py
+++ square.py
@@ -1,12 +1,11 @@
 class Square():
-    _file = ""
-    _column = ""
+    _file = 0
+    _column = 0
 
     def __init__(self, column = 0, file = 0) -> None:
         self._file = file
 
     def get_coordinates(self):
-        if self._file == 0:
-            return "a1"
+        file_coordinate = self._file + 1
         
-        return "a2"
+        return "a" + str(file_coordinate)
--- tests/test_squares.py
+++ tests/test_squares.py
@@ -9,4 +9,7 @@ def test_first_square_is_a1():
 def test_TheSecondSquare_OnFirstColumn_is_a2():
     assert Square(0,1).get_coordinates() == "a2"
 
+def test_TheSeventhSquare_OnFirstColumn_is_a7():
+    assert Square(0,6).get_coordinates() == "a7"
+
 
