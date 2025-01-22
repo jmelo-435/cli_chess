4d1dd7010794ca8c8935e3748754c0f624ba3b36
Jeferson Douglas <jefersonmedeiros000@gmail.com>
2025-01-22 20:27:34 -0300

    Refactoring: Board
    String representation becomes an atribute
    
    Signed-off-by: Jeferson Douglas <jefersonmedeiros000@gmail.com>

--- board/board.py
+++ board/board.py
@@ -2,6 +2,7 @@ from .square import Square
 
 class Board():
     squares = []
+    _representation = ""
 
     def __init__(self) -> None:
         i=0
@@ -16,30 +17,29 @@ class Board():
         
         return next(square for square in self.squares if square.get_coordinates() == coordinate)  
     
-    def _add_foot_coordinates(self,board):
+    def _add_foot_coordinates(self):
         footer = "     a       b       c       d       e       f       g       h    "
-        return board + footer
+        return self._representation + footer
 
     def toString(self):
         i = 0
-        representation = ""
 
         while i<8:
             k=0
             while k <4:
                 if k ==0:
-                    representation += (str(8-i)+" ")
+                    self._representation += (str(8-i)+" ")
                 else:
-                    representation += ("  ")
+                    self._representation += ("  ")
                 j = 0
                 while j<8:
                     square = next(square for square in self.squares if square.get_numeral_coordinates() == (j,i))
-                    representation += square.print()
+                    self._representation += square.print()
                     j+=1
                 k+=1
             i+=1
         
-        return self._add_foot_coordinates(representation)
+        return self._add_foot_coordinates()
 
         
     
