3e08a83bf2a91b43b6b504f0d0e627078852b7e5
Jeferson Douglas <jefersonmedeiros000@gmail.com>
2025-01-22 22:18:39 -0300

    Implements: Piece
    Enforces a Piece collor definition interface
    
    Signed-off-by: Jeferson Douglas <jefersonmedeiros000@gmail.com>

--- piece/piece.py
+++ piece/piece.py
@@ -1,3 +1,9 @@
 
 class Piece():
-    pass
+    _isWhite = True
+
+    def __init__(self,isWhite= True) -> None:
+        self._isWhite = isWhite
+    
+    def isWhite(self):
+        return self._isWhite
--- tests/test_pices.py
+++ tests/test_pices.py
@@ -1,3 +1,10 @@
 from ..piece.piece import Piece
 
 piece = Piece()
+
+def test_piece_has_color():
+    piece.isWhite() == True
+
+def test_piece_color_is_defined_on_initialization():
+    blackPiece = Piece(isWhite=False)
+    blackPiece.isWhite() == False
